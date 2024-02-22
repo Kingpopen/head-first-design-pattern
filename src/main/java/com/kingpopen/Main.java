eef944c592a9c9a6dc79860de89833b0f7afa015
kingpopen <kingpopen@163.com>
2024-02-22 23:03:17 +0800

    项目初始化

---
+++ src/main/java/com/kingpopen/Main.java
@@ -0,0 +1,13 @@
+package com.kingpopen;
+
+/**
+ * @author 彭锦波
+ * @project Default (Template) Project
+ * @description ${description}
+ * @date 2024/02/19 22:04:36
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
